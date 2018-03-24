7ba5e0a423aaeeb28b110b09145551638adb335d
wararyo <wararyo@gmail.com>
2018-03-24 20:51:34 +0900

    Main基礎(ブランチ間違えた)

---
+++ Made_In_Imp/Assets/Commander/MainManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (coroutineWork);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator coroutineWork(){
+		yield new WaitForSecondsRealtime(8);
+	}
+}
