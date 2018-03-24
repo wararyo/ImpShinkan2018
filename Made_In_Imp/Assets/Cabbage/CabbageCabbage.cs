a8e996fadb14aa6ead5f30e5d970ccda66140442
wararyo <wararyo@gmail.com>
2018-03-24 15:52:33 +0900

    キャベツ基礎

---
+++ Made_In_Imp/Assets/Cabbage/CabbageCabbage.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CabbageCabbage : MonoBehaviour {
+
+	int cutCount = 0;
+	[System.NonSerialized()]
+	public int cutCountGoal = 40;
+	public UnityEvent onCutAll;
+
+	bool isCutAll = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Cut() {
+		cutCount++;
+		if (cutCountGoal <= cutCount) {
+			if (isCutAll == false) {
+				onCutAll.Invoke ();
+				isCutAll = true;
+			}
+		}
+	}
+}
---
+++ Made_In_Imp/Assets/Cabbage/CabbageManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CabbageManager : MonoBehaviour {
+
+	public int cutCountGoal = 40;
+	[Space()]
+
+	public CabbageCabbage cabbage1P;
+	public CabbageCabbage cabbage2P;
+
+	// Use this for initialization
+	void Start () {
+		cabbage1P.cutCountGoal = cutCountGoal;
+		cabbage2P.cutCountGoal = cutCountGoal;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("1A")) {
+			cabbage1P.Cut();
+		}
+	}
+
+	public void OnCutSucceed1P() {
+
+	}
+
+	public void OnCutSucceed2P() {
+
+	}
+}
