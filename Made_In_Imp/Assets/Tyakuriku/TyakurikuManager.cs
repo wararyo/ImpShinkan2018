4902d4067b9e0e00a510c43fa76897817e4e8fd8
naotee917 <naotee917@gmail.com>
2018-03-27 15:17:14 +0900

    地球を回した

--- Made_In_Imp/Assets/Tyakuriku/TyakurikuManager.cs
+++ Made_In_Imp/Assets/Tyakuriku/TyakurikuManager.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class TyakurikuManager : MonoBehaviour {
 
+    public GameObject ground1, ground2;
+    public float rotateSpeed = 0.1f;
+    private Vector3 rotateAngle;
+    
 	// Use this for initialization
 	void Start () {
-		
+        rotateAngle = new Vector3(0, 0, rotateSpeed);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        RotateGround();
+
 	}
+
+    public void RotateGround()
+    {
+        ground1.transform.Rotate(rotateAngle);
+        ground2.transform.Rotate(rotateAngle);
+    }
 }
