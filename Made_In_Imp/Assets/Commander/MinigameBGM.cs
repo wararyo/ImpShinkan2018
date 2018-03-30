f2a9c775b7dd072ddd0598a4cf55df4a4242def0
wararyo <wararyo@gmail.com>
2018-03-30 15:45:53 +0900

    マックBGM&汎用的なBGMプレハブ(ブランチ間違えました)

---
+++ Made_In_Imp/Assets/Commander/MinigameBGM.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MinigameBGM : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Commander.onMinigameStart += () => {
+			GetComponent<AudioSource>().Play();
+		};
+		Commander.onMinigameEnd += () => {
+			GetComponent<AudioSource>().Stop();
+		};
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
