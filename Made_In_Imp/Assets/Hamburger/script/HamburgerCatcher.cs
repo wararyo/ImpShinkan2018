0ac8b9f168a0f2d46550a6a04154436ccd4c5c3f
wararyo <wararyo@gmail.com>
2018-03-31 14:30:43 +0900

    ハンバーガー難易度調整

--- Made_In_Imp/Assets/Hamburger/script/HamburgerCatcher.cs
+++ Made_In_Imp/Assets/Hamburger/script/HamburgerCatcher.cs
@@ -14,7 +14,12 @@ public class HamburgerCatcher : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         //移動処理　画面からはみ出ないよ！
-        float v = Input.GetAxis((playerNum + 1).ToString() + "Horizontal") * 0.5f;
+        float v = Input.GetAxis((playerNum + 1).ToString() + "Horizontal");
+		if (Input.GetButton ((playerNum + 1).ToString () + "L"))
+			v = -1.2f;
+		else if(Input.GetButton ((playerNum + 1).ToString () + "R"))
+			v = 1.2f;
+		v *= 0.4f;
         float pos_x = transform.position.x;
         if (Mathf.Abs(pos_x + v) > 4.8f) v = 0;
         transform.position += Vector3.right * v;
--- Made_In_Imp/Assets/Hamburger/script/HamburgerManager.cs
+++ Made_In_Imp/Assets/Hamburger/script/HamburgerManager.cs
@@ -13,6 +13,9 @@ public class HamburgerManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         StartCoroutine(GameCoroutine());
+		Commander.onMinigameEnd += () => {
+			JudgeResult();//全部具材が乗ったか判定 
+		};
 	}
 	
 	// Update is called once per frame
@@ -25,21 +28,23 @@ public class HamburgerManager : MonoBehaviour {
         var order = new int[] { 0, 1, 2, 3, 4, 5, 6 }.OrderBy(i => System.Guid.NewGuid()).Concat(new int[] { 7 }).ToArray();
         yield return new WaitForSeconds(0.5f);
         for (int i = 0; i < order.Length; i++) {
-            ProvideGu(order[i]);
+            ProvideGu(order[i], i);
             yield return new WaitForSeconds(0.8f);　//一定間隔に...
         }
-        JudgeResult(); //全部具材が乗ったか判定
+        //JudgeResult(); //全部具材が乗ったか判定
     }
 
-    void ProvideGu(int index) {
+    void ProvideGu(int index, int layer) {
+		
         //具生成
-        Vector3 pos = new Vector3(Random.Range(-4, 4), 6, 0);
+        Vector3 pos = new Vector3(Random.Range(-3.6f, 3.6f), 6, 0);
         GameObject obj = Instantiate(gu_obj, transform);
         obj.layer = gameObject.layer;
         obj.transform.position = pos;
         var sr = obj.GetComponent<SpriteRenderer>();
         sr.sprite = gu_sprites[index];
         var col = obj.GetComponent<BoxCollider2D>();
+		sr.sortingOrder = 10 + layer;
         //具の大きさによって当たり判定調整
         col.size = new Vector2(sr.bounds.size.x * 0.7f, sr.bounds.size.y * 0.7f);
         col.offset += Vector2.up * sr.bounds.size.y * 0.2f;
